2958f393c09918a7245355f24b7e3a3091eae6df
Jey <63811758+JeyCraft@users.noreply.github.com>
2023-04-27 18:51:01 +0200

    de.lua
    German Translations

---
+++ locales/translations/de.lua
@@ -0,0 +1,9 @@
+Language["de"] = {
+    scanner_title = "Metall Detektor",
+    metal_detect = "Metall gefunden!",
+    metal_none = "Kein Metall gefunden.",
+    no_player = "Kein Spieler in der Nähe.",
+    wand_title = "Metall Detektor",
+    wand_search = "Scanne nach Metall...",
+    group_denied = "Das kannst du nicht tun.",
+}
