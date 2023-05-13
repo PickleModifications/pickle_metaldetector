c9a1d5bb6e204ae96cc3366cadc91a4c42421dfd
nzkfc <nzryancas@gmail.com>
2023-05-13 18:55:35 +1200

    Update server.lua
    Fixes nil error due to config reference mismatch

--- modules/wand/server.lua
+++ modules/wand/server.lua
@@ -2,10 +2,10 @@ RegisterNetEvent("pickle_metaldetector:searchWand", function(target)
     local source = source
     if not CanAccessGroup(source, Config.Wand.groups) then return ShowNotification(source, _L("group_denied")) end
     local contraband = {}
-    for i=1, #Config.Scanner.items do 
-        local count = Search(target, Config.Scanner.items[i])
+    for i=1, #Config.MetalItems do 
+        local count = Search(target, Config.MetalItems[i])
         if count > 0 then 
-            table.insert(contraband, {label = GetItemLabel(Config.Scanner.items[i]), count = count})
+            table.insert(contraband, {label = GetItemLabel(Config.MetalItems[i]), count = count})
         end
     end
     TriggerClientEvent("pickle_metaldetector:detectWand", -1, source, contraband)
